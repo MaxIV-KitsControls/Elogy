f3a29d81f11675e4f23fe08cbc39ea08eeb7d3cd
Johan Forsberg <johan.forsberg@maxiv.lu.se>
2017-03-25 18:52:17 +0100

    update setup script with authorship details

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 setup(
     name='elogy',
+    author='Johan Forsberg',
+    author_email='johan@slentrian.org',
+    url='https://github.com/johanfforsberg/elogy',
     packages=['elogy'],
     include_package_data=True,
     install_requires=[
