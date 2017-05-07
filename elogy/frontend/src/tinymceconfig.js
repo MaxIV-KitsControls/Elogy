2f01f813a7234dbf6e7725c1163e12121927eabd
Johan Forsberg <johan.forsberg@maxiv.lu.se>
2017-05-08 00:12:24 +0200

    add missing file

---
+++ elogy/frontend/src/tinymceconfig.js
@@ -0,0 +1,32 @@
+// configuration for the editor
+const TINYMCE_CONFIG = {
+    plugins: "link image textcolor paste table lists advlist code",
+    toolbar: (
+        "undo redo | removeformat | styleselect |"
+        + " bold italic forecolor backcolor |"
+        + " bullist numlist outdent indent | link image table | code"
+    ),
+    menubar: false,
+    statusbar: false,
+    content_css: "/static/tinymce-tweaks.css",
+    height: "100%",
+    relative_urls : false,  // otherwise images broken in editor
+    apply_source_formatting: false,
+    force_br_newlines: false,
+    paste_data_images: true,
+    //          automatic_uploads: false,  // don't immediately upload images
+    //images_upload_handler: customUploadHandler,
+    image_dimensions: false,
+    forced_root_block : "",
+    cleanup: true,
+    force_p_newlines : true,                             
+    convert_newlines_to_brs: false,                             
+    inline_styles : false,
+    entity_encoding: 'raw',
+    entities: '160,nbsp,38,amp,60,lt,62,gt',
+    resize: true,
+    theme: "modern"
+}    
+
+
+export default TINYMCE_CONFIG;
