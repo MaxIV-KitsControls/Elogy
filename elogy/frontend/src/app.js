e21744bb7936efac7a01e2b4ceae9bba98509495
Johan Forsberg <johan.forsberg@maxiv.lu.se>
2017-05-15 23:19:34 +0200

    small routing fixes

--- elogy/frontend/src/app.js
+++ elogy/frontend/src/app.js
@@ -13,7 +13,7 @@ import LogbookTree from './logbooktree.js';
 import QuickSearch from './search.js';
 import SearchResults from './searchresults.js';
 import EventBus from './eventsystem.js';
-import {withProps} from './util.js';
+import { withProps } from './util.js';
 import './app.css';
 
 
@@ -23,31 +23,40 @@ import './app.css';
 // data around we'd better switch to Redux or something.
 const eventbus = new EventBus();
 
+// wrap the relevant component with the eventbus as a prop
+// This is the only (?) way to send props to a route component
+const LogbookTreeWithEventbus = withProps(LogbookTree, {eventbus});
+const LogbookWithEventbus = withProps(Logbook, {eventbus});
+const EntryEditorWithEventbus = withProps(EntryEditor, {eventbus});
+const LogbookEditorWithEventbus = withProps(LogbookEditor, {eventbus});
 
 const Elogy = () => (
 
+    /* Set up a browser router that will render the correct component
+       in the right places, all depending on the current URL.  */
+    
     <Router>
         <div id="app">
             
             <div id="logbooks">
                 <Switch>
                     <Route path="/logbooks/:logbookId"
-                           component={withProps(LogbookTree, {eventbus})}/>
-                    <Route component={withProps(LogbookTree, {eventbus})}/>
+                           component={ LogbookTreeWithEventbus }/>
+                    <Route component={ LogbookTreeWithEventbus }/>
                 </Switch>
                 <Switch>
                     <Route path="/logbooks/:logbookId"
-                           component={QuickSearch}/>
-                    <Route component={QuickSearch}/>
+                           component={ QuickSearch }/>
+                    <Route component={ QuickSearch }/>
                 </Switch>                            
             </div>
             
             <div id="logbook">
                 <Switch>                            
                     <Route path="/logbooks/:logbookId/entries/:entryId"
-                           component={withProps(Logbook, {eventbus})}/>
+                           component={ LogbookWithEventbus }/>
                     <Route path="/logbooks/:logbookId"
-                           component={withProps(Logbook, {eventbus})}/>
+                           component={ LogbookWithEventbus }/>
                 </Switch>
             </div>
 
@@ -55,19 +64,17 @@ const Elogy = () => (
                 <Switch>
 
                     <Route path="/logbooks/:logbookId/entries/new"
-                           component={withProps(EntryEditor, {eventbus})}/>
+                           component={EntryEditorWithEventbus}/>
                     <Route path="/logbooks/:logbookId/entries/:entryId/:command"
-                           component={withProps(EntryEditor, {eventbus})}/>
+                           component={EntryEditorWithEventbus}/>
                     
                     <Route path="/logbooks/:logbookId/entries/:entryId"
                            component={Entry}/>
-                    <Route path="/entries/:entryId"
-                           component={Entry}/>
 
                     <Route path="/logbooks/new"
-                           component={withProps(LogbookEditor, {eventbus})}/>
+                           component={LogbookEditorWithEventbus}/>
                     <Route path="/logbooks/:logbookId/:command"
-                           component={withProps(LogbookEditor, {eventbus})}/>
+                           component={LogbookEditorWithEventbus}/>
                     
                 </Switch>
             </div>
