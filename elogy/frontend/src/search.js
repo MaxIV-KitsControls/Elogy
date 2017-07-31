b0bf6cde58ee31d9859d86f26e792c7c2bae2725
Johan Forsberg <johan.forsberg@maxiv.lu.se>
2017-07-31 14:47:03 +0200

    add recursive checkbox to search

--- elogy/frontend/src/search.js
+++ elogy/frontend/src/search.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { parseQuery } from "./util.js";
+import "./search.css";
 
 
 class QuickSearch extends React.Component {
@@ -11,7 +12,8 @@ class QuickSearch extends React.Component {
             content: null,
             title: null,
             authors: null,
-            attachments: null
+            attachments: null,
+            ignore_children: false
         }
     }
 
@@ -19,6 +21,10 @@ class QuickSearch extends React.Component {
         this.setState({[event.target.name]: event.target.value});
     }
 
+    onToggleChildLogbooks (event) {
+        this.setState({ignore_children: !this.state.ignore_children});
+    }
+    
     onSubmit (history, event) {
         event.preventDefault();  // skip the normal server form submit
 
@@ -28,56 +34,67 @@ class QuickSearch extends React.Component {
         // and sharing.
         const query = Object.keys(this.state)
                             .filter(key => this.state[key])
-                            .map(key => `${key}=${this.state[key]}`)
-                            .join("&");
+                            .map(key => `${key}=${this.state[key]}`);
 
         // The parent parameter also needs to be kept around because it
         // is used to limit the set of visible logbooks.
         const parent = parseQuery(this.props.location.search)["parent"];
-        const parentQuery = parent? `parent=${parent}&` : "";
+        if (parent) {
+            query.push(`parent=${parent}`);
+        }
         history.push({
             pathname: `/logbooks/${this.props.match.params.logbookId || 0}`,
-            search: `?${parentQuery}${query}`,
+            search: `?${query.join("&")}`,
             state: this.state
         });
     }
 
     onReset (history, event) {
-        this.setState({content: null, title: null, authors: null, attachments: null},
+        this.setState({content: null, title: null, authors: null,
+                       attachments: null, ignore_children: false},
                       this.onSubmit.bind(this, history, event));
     }
     
     render () {
 
-        console.log("search", this.props)
-        
         return (
             // This is a slightly ugly way of injecting the browser history
             // into the component so that we can navigate programmatically
             // when the form is submitted...
             <Route render={({history}) => (
                 <form id="search" onSubmit={this.onSubmit.bind(this, history)}>
-
+                    
                     <input style={{width: "100%"}} name="title"
                                  value={this.state.title || ""}
+                                 title="Filter entries on title (supports regular expressions)"
                                  type="text" ref="title" placeholder="Title"
                                  onChange={this.onChange.bind(this)}/>
                     <input style={{width: "100%"}} name="content"
                                  value={this.state.content || ""}
+                                 title="Filter entries on contents (supports regular expressions)"
                                  type="text" ref="content"
                                  placeholder="Content"      
                                  onChange={this.onChange.bind(this)}/>
                     <input style={{width: "100%"}} name="authors"
                                  value={this.state.authors || ""}
+                                 title="Filter entries on authors (supports regular expressions)"
                                  type="text" ref="authors" placeholder="Authors"
                                  onChange={this.onChange.bind(this)}/>
                     <input style={{width: "100%"}} name="attachments"
                                  value={this.state.attachments || ""}
+                                 title="Filter entries on attachments (supports regular expressions)"
                                  type="text" ref="attachments" placeholder="Attachments"
                                  onChange={this.onChange.bind(this)}/>
-                    
-                    <input type="submit" value="Search"/>
-                    <input type="button" value="Clear" onClick={this.onReset.bind(this, history)}/>
+                    <label title="Whether to include entries in logbooks contained in the selected logbook.">
+                        <input type="checkbox"
+                                     checked={!this.state.ignore_children}
+                                     onChange={this.onToggleChildLogbooks.bind(this)}/>
+                        Recursive
+                    </label>
+                    <fieldset>
+                        <input type="submit" value="Search"/>
+                        <input type="button" value="Clear" onClick={this.onReset.bind(this, history)}/>
+                    </fieldset>
                 </form>
             )}/>
         );
