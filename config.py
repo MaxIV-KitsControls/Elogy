2562dfc625b94360ea82f138fdd659435d06349e
Johan Forsberg <johan.forsberg@maxiv.lu.se>
2017-09-29 17:21:12 +0200

    example of email sending

--- config.py
+++ config.py
@@ -30,15 +30,34 @@ def new_entry(data):
     # into a dict (same format as the JSON api). We don't want user
     # scripts to accidentally modify the database...
 
-    # It's OK to do potentially slow stuff here such as network
+    # Should be OK to do potentially slow stuff here such as network
     # calls, since actions are run in a thread. But make sure it
     # terminates sooner or later or you will have a resource leak!
 
     # Some example actions:
     if "Mailto" in entry["attributes"]:
-        # Send an email to entry.attributes["Mailto"],
-        # perhaps using the "smtplib" module?
-        pass
+
+        # Send an email to the address(es) given in the "Mailto" attribute
+
+        # TODO: inline images aren't displayed since they are just
+        #       relative links. Might be a good idea to inline them.
+        # TODO: shouldn't assume messages are HTML
+        # TODO: include entry authorship information?
+
+        import logging
+        from smtplib import SMTP
+        from email.mime.text import MIMEText
+
+        fromaddr = "elogy@maxiv.lu.se"
+        toaddrs = entry["attributes"]["Mailto"]
+        content = "<html> {} </html>".format(entry["content"])
+        message = MIMEText(content, "html")
+        message["Subject"] = entry["title"]
+        message["From"] = fromaddr
+        message["To"] = toaddrs
+        with SMTP("smtp.maxiv.lu.se", 25) as smtp:
+            logging.info(smtp.send_message(message))
+
     if "Ticket" in entry["attributes"]:
         # Create a ticket in your ticket system
         pass
@@ -49,10 +68,10 @@ def edit_logbook(data):
 
 
 ACTIONS = {
-    "new_entry": new_entry,
+    "new_entry": None,  # new_entry,
     "edit_entry": None,
     "new_logbook": None,
-    "edit_logbook": edit_logbook
+    "edit_logbook": None  # edit_logbook
 }
 
 
